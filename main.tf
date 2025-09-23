35eed0e0f4bc228f8f494ee6f3d123b180e97c27
Rahulsingh0007 <48233043+Rahulsingh0007@users.noreply.github.com>
2025-09-23 18:16:32 +0530

    Update main.tf
    removed provider

--- main.tf
+++ main.tf
@@ -1,8 +1,4 @@
 # Configure the AWS provider
-provider "aws" {
-  region = "us-east-1"   # Change region as needed
-}
-
 # Create S3 bucket
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "omer-terraform-bucket-12345"   # Must be globally unique
