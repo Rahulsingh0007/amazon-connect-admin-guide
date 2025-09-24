c1a006efa6cfcb935c80082a468c291e5a8fdb18
Rahulsingh0007 <48233043+Rahulsingh0007@users.noreply.github.com>
2025-09-24 15:23:08 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,6 @@
 terraform {
   # Require Terraform v1.13.3 or higher
   required_version = ">= 1.13.3"
-
   # Use Terraform Cloud for state management
   cloud {
     organization = "AWS_connect_terraform"   # 👈 your Terraform Cloud org name
@@ -10,7 +9,6 @@ terraform {
       name ="amazon-connect-admin-guide"   # 👈 your workspace name
     }
   }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -18,7 +16,6 @@ terraform {
     }
   }
 }
-
 provider "aws" {
   region = "ap-southeast-1"   # 👈 change to your desired AWS region
 }
